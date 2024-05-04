2a4f28cf6049e999e17f7d910f337402f03a1f88
Марат Лайшев <laischevmarat@yandex.ru>
2024-05-04 05:43:04 +0300

    db

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -79,12 +79,12 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 #     }
 # }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 AUTH_PASSWORD_VALIDATORS = [
